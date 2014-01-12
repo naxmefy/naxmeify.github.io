8deb33373aa72a57d843616cf6e09bc9d52d2f33
Naxmeify <info@nax.me>
2014-01-12 20:48:22 +0100

    angular
    partials
    css
    routes
    loc

---
+++ js/app/app.js
@@ -0,0 +1,43 @@
+﻿'use strict';
+var app = angular.module('naxmeify', []).
+  config(['$routeProvider', function ($routeProvider) {
+      $routeProvider.
+          when('/', { templateUrl: 'partials/index.html', activetab: 'home', controller: HomeCtrl }).
+          when('/about', {
+              templateUrl: 'partials/about.html',
+              controller: AboutCtrl,
+              activetab: 'about'
+          }).
+          when('/work', {
+              templateUrl: 'partials/work.html',
+              controller: WorkCtrl,
+              activetab: 'work'
+          }).
+          when('/contact', {
+              templateUrl: 'partials/contact.html',
+              controller: ContactCtrl,
+              activetab: 'contact'
+          }).
+          otherwise({ redirectTo: '/' });
+  }]).run(['$rootScope', '$http', '$browser', '$timeout', "$route", function ($scope, $http, $browser, $timeout, $route) {
+
+    
+      $scope.$on("$routeChangeSuccess", function (scope, next, current) {
+          $scope.part = $route.current.activetab;
+          $scope.transitionState = "active";
+      });
+
+
+      $scope.save = function () {
+          $scope.loaded = true;
+          $scope.process = true;
+          $http.post('/home/sendemail', $scope.message).success(function () {
+              $scope.success = true;
+              $scope.process = false;
+          });
+      };
+  }]);
+
+app.config(['$locationProvider', function($location) {
+    $location.hashPrefix('!');
+}]);
---
+++ js/app/controllers.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+function HomeCtrl($scope, $http) {
+  
+}
+
+function WorkCtrl($scope, $http) {
+    
+}
+
+function AboutCtrl($scope, $http, $timeout) {
+   
+}
+function ContactCtrl($scope, $http, $timeout) {
+   
+}
