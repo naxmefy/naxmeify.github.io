3dd7c8f96ae6f9060c461e69d0669366c5769364
Nax (aka Matt) <m.neundorf@live.de>
2015-05-29 10:00:04 +0000

    improved jquery search for skills and works, added dummy works, improved work view (#8)

--- js/jquery.fix.js
+++ js/jquery.fix.js
@@ -9,14 +9,16 @@ String.prototype.capitalizeFirstLetter = function() {
   return this.charAt(0).toUpperCase() + this.slice(1);
 };
 
-/** FOR SKILLS VIEW */
+/** FOR FILTERED VIEW */
+
+
 jQuery(function() {
-  if($('#skills').length > 0) {
-    $('#skillsFilter').keyup(function(event) {
+  function setupFilterInput(id) {
+    $('#'+id+'Filter').keyup(function(event) {
       event.preventDefault();
       var element = event.currentTarget;
       var value = $(element).val().toLowerCase();
-      $('#skills .skill').each(function(index, element) {
+      $('#'+id+' .item').each(function(index, element) {
         var name = $(element).find('.name').text().toLowerCase();
         if(value.length > 0) {
           if((name.indexOf(value) !== -1)) {
@@ -30,4 +32,43 @@ jQuery(function() {
       });
     });
   }
+
+  if($('#skills').length > 0) {
+    setupFilterInput('skills');
+  }
+  
+  if($('#works').length > 0) {
+    setupFilterInput('works');
+    
+    // Synchronize height of works
+    var thumbnailHeight = 0;
+    var headingHeight = 0;
+    var textHeight = 0;
+    var leadHeight = 0;
+    
+    $.each($('#works .item'), function(index, element) {
+      //var thumbnailElement = $(element).find('.thumbnail');
+      var headingElement = $(element).find('.name');
+      var textElement = $(element).find('.text');
+      var leadElement = $(element).find('.tags');
+      
+      //thumbnailHeight = thumbnailElement.height() > thumbnailHeight ? thumbnailElement.height() : thumbnailHeight;
+      headingHeight = headingElement.height() > headingHeight ? headingElement.height() : headingHeight;
+      textHeight = textElement.height() > textHeight ? textElement.height() : textHeight;
+      leadHeight = leadElement.height() > leadHeight ? leadElement.height() : leadHeight;
+    });
+    
+    setTimeout(function() {
+      //console.log(thumbnailHeight);
+      console.log(headingHeight);
+      console.log(textHeight);
+      console.log(leadHeight);
+      
+      //$('.gridview .item .thumbnail').height(thumbnailHeight);
+      $('#works .item .name').height(headingHeight);
+      $('#works .item .text').height(textHeight);
+      $('#works .item .tags').height(leadHeight);
+    }, 10);
+  }
+  
 });
