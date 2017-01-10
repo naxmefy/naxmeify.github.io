9fb7a83b9714bf3235998c7299a26918cc4b9938
MRW Neundorf <m.neundorf@live.de>
2017-01-10 10:02:07 +0100

    update latest github activities component

--- js/app/components/latest-github-activities.js
+++ js/app/components/latest-github-activities.js
@@ -2,7 +2,8 @@ angular.module('app')
   .component('latestGithubActivities', {
     template: '<div class="list-group">' +
     '<h4><a href="{( $ctrl.link )}">Latest Github Activities</a></h4>' +
-    '<a class="list-group-item" ng-repeat="pen in $ctrl.pens | limitTo:10" ng-href="pen.link" target="_blank">{(pen.title)}</a>' +
+    '<a class="list-group-item" ng-repeat="activity in $ctrl.activities | limitTo:10" ' +
+    'ng-href="activity.link" target="_blank">{(activity.title)}</a>' +
     '</div>',
     controller: [
       'FeedService',
@@ -10,7 +11,7 @@ angular.module('app')
         FeedService.parseFeed('https://github.com/naxmefy.atom')
           .then(function (response) {
             this.link = response.data.responseData.feed.link;
-            this.pens = response.data.responseData.feed.entries;
+            this.activities = response.data.responseData.feed.entries;
           }.bind(this))
       }
     ]
