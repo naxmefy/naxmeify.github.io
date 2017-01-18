c17987561c477e8c737a47c3d4ba28c7e23f2868
MRW Neundorf <m.neundorf@live.de>
2017-01-18 09:48:39 +0100

    update feed

--- js/app/services/feed.js
+++ js/app/services/feed.js
@@ -3,7 +3,12 @@ angular.module('app')
   '$http', function ($http) {
     return {
       parseFeed: function (url) {
-        return $http.get('https://api.rss2json.com/v1/api.json?rss_url='+ encodeURIComponent(url));
+        return $http.get('https://api.rss2json.com/v1/api.json?' +
+          'api_key=jh6yds6n6pdzajcwbx7inxxey7ju5sgqel3srufw' +
+          'order_dor=desc' +
+          'count=10' +
+          'rss_url=' +
+          ''+ encodeURIComponent(url));
       }
     }
   }]);
